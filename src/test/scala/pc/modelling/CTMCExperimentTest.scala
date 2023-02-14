853324cfe018391c84a4b4ed02152517fd540541
Elena Yan <elena.yan@studio.unibo.it>
2023-02-14 11:00:52 +0100

    test: steady state

--- src/test/scala/pc/modelling/CTMCExperimentTest.scala
+++ src/test/scala/pc/modelling/CTMCExperimentTest.scala
@@ -9,16 +9,26 @@ import java.util.Random
 class CTMCExperimentTest extends AnyFunSuite:
 
   enum State:
-    case IDLE, DONE
+    case S0, S1, S2
 
   export State.*
   export pc.modelling.CTMCExperiment.*
   export pc.modelling.CTMCSimulation.*
 
   def simpleChannel: CTMC[State] = CTMC.ofTransitions(
-    Transition(DONE, 1.0 --> DONE)
+    Transition(S0, 1.0 --> S0)
   )
 
-  test("All states of the trace should be globally DONE") {
-    simpleChannel.globally[State](_ == DONE)(simpleChannel.newSimulationTrace(DONE, new Random).take(50)) shouldBe true
+  def channel: CTMC[State] = CTMC.ofTransitions(
+    Transition(S0, 0.001 --> S1),
+    Transition(S1, 1 --> S2),
+    Transition(S2, 1 --> S1)
+  )
+
+  test("All states of the trace should be globally S0") {
+    simpleChannel.globally[State](_ == S0)(simpleChannel.newSimulationTrace(S0, new Random).take(50)) shouldBe true
+  }
+
+  test("The steady-state probability in the channel that the state in S1 should be about 50%") {
+    channel.steadyState[State](_ == S1)(channel.newSimulationTrace(S0, new Random).take(1000)) shouldBe (0.5 +- 0.1)
   }
